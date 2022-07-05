aac539bd1469f243468b4241d23e1b18156ce778
xanqus <sbk98031816@gmail.com>
2022-07-06 05:41:58 +0900

    EventSource 객체 사용하기

--- js/chat.js
+++ js/chat.js
@@ -1,7 +1,17 @@
+const eventSource = new EventSource(
+  'http://localhost:8083/sender/xanqus/receiver/jhs512'
+)
+
+eventSource.onmessage = e => {
+  console.log('e', e)
+  const data = JSON.parse(e.data)
+  console.log('data', data)
+}
+
 let chatBox = document.querySelector('#chat-box')
 let msgInput = document.querySelector('#chat-outgoing-msg')
 
-const getMsg = msg => {
+const getSendMsgBox = msg => {
   return `<div class="sent_msg">
     <p>${msg}</p>
     <span class="time_date"> 11:18 | Today</span>
@@ -11,7 +21,7 @@ const getMsg = msg => {
 document.querySelector('#chat-send').addEventListener('click', () => {
   let chatOutgoingBox = document.createElement('div')
   chatOutgoingBox.className = 'outgoing_msg'
-  chatOutgoingBox.innerHTML = getMsg(msgInput.value)
+  chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value)
   chatBox.append(chatOutgoingBox)
   msgInput.value = ''
 })
@@ -20,7 +30,7 @@ document.querySelector('#chat-outgoing-msg').addEventListener('keydown', e => {
   if (e.keyCode === 13) {
     let chatOutgoingBox = document.createElement('div')
     chatOutgoingBox.className = 'outgoing_msg'
-    chatOutgoingBox.innerHTML = getMsg(msgInput.value)
+    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value)
     chatBox.append(chatOutgoingBox)
     msgInput.value = ''
   }
