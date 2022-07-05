80923f160a8db206eaff3f2e40efeafe35e65a82
xanqus <sbk98031816@gmail.com>
2022-07-06 06:00:21 +0900

    기존 채팅 내역 출력하기

--- js/chat.js
+++ js/chat.js
@@ -6,32 +6,53 @@ eventSource.onmessage = e => {
   console.log('e', e)
   const data = JSON.parse(e.data)
   console.log('data', data)
+  initMessage(data)
 }
 
 let chatBox = document.querySelector('#chat-box')
 let msgInput = document.querySelector('#chat-outgoing-msg')
 
-const getSendMsgBox = msg => {
+const getSendMsgBox = (msg, time) => {
   return `<div class="sent_msg">
     <p>${msg}</p>
-    <span class="time_date"> 11:18 | Today</span>
+    <span class="time_date"> ${time} </span>
   </div>`
 }
 
-document.querySelector('#chat-send').addEventListener('click', () => {
+const initMessage = data => {
+  const { msg, createdAt } = data
   let chatOutgoingBox = document.createElement('div')
   chatOutgoingBox.className = 'outgoing_msg'
-  chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value)
+  chatOutgoingBox.innerHTML = getSendMsgBox(msg, createdAt)
   chatBox.append(chatOutgoingBox)
   msgInput.value = ''
+}
+
+const addMessage = () => {
+  let chatOutgoingBox = document.createElement('div')
+  chatOutgoingBox.className = 'outgoing_msg'
+
+  let date = new Date()
+  let now =
+    date.getHours() +
+    ':' +
+    date.getMinutes() +
+    ' | ' +
+    (date.getMonth() + 1) +
+    '/' +
+    date.getDate()
+
+  chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value, now)
+  chatBox.append(chatOutgoingBox)
+  msgInput.value = ''
+}
+
+document.querySelector('#chat-send').addEventListener('click', () => {
+  addMessage()
 })
 
 document.querySelector('#chat-outgoing-msg').addEventListener('keydown', e => {
   if (e.keyCode === 13) {
-    let chatOutgoingBox = document.createElement('div')
-    chatOutgoingBox.className = 'outgoing_msg'
-    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value)
-    chatBox.append(chatOutgoingBox)
-    msgInput.value = ''
+    addMessage()
   }
 })
